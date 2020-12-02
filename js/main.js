4102faf1d05d582ee7e327e48b99a9f4f3ff1911
grja04 <grja04@gmail.com>
2020-12-01 22:47:39 -0600

    primera parte

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.getElementById("botonSecundario").addEventListener("click", () => {
+console.log("click")
+let name = document.getElementById("First-name").value
+let lastName = document.getElementById("Last-name").value
+console.log(name, lastName)
+
+let userObject = {name, lastName}
+console.log( userObject )
+userArray.push (userObject)
+console.log(userArray)
+addUserToList();
+
+})
+
+const addUserToList = () => { 
+   document.getElementById("user-list").innerHTML = ""
+   userArray.forEach( user => {
+       let currentContent = document.getElementById("user-list").innerHTML
+       let newItem = `
+        <li class= "list-group"> ${ user.name } ${user.lastName} </li>`
+
+       let newContent = currentContent + newItem
+
+       document.getElementById("user-list").innerHTML = newContent
+   })
+}
